edc99e86018fbe633f6c3cc1da41ce50198e63ca
Susanelizabetth <castillosusana55@gmail.com>
2022-11-10 12:34:08 -0600

    apply model in a endpoin

--- apiml.py
+++ apiml.py
@@ -4,10 +4,33 @@ import pickle
 import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
 
-@app.get('/')
-async def scoring_endpoint():
-    return {"name": "hello world"}
+class ScoringItem(BaseModel):
+    albumina: float
+    globulos_blancos: int
+    creatinina: float
+    hipertension: int
+    diabetes_mellitus: float
+
+
+class ScoringItems(BaseModel):
+    List[ScoringItem]
+
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+
+@app.post('/')
+async def scoring_endpoint(item: List[ScoringItem]):
+    data = []
+    for i in item:
+        data.append(i.dict())
+    df = pd.DataFrame(data)
+    yhat = model.predict(df)
+
+    return {"prediction": yhat.tolist()}
